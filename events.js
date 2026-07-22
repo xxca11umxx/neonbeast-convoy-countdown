b1201b58c66c5a0fa50c978fdccde222dd028903
xxca11umxx <theeabros17@gmail.com>
2026-07-22 05:33:09 +0100

    Automatic convoy update

--- events.js
+++ events.js
@@ -1,1 +1,10 @@
-
+const EVENTS = [
+    {
+        "title": "Imperial Cargo  ETS2  Convoy  To be determined  8  3",
+        "date": "2026-07-29T13:00:00",
+        "server": "To be determined",
+        "route": "Check TruckersMP Event",
+        "banner": "",
+        "link": "https://truckersmp.com/events/35582-%F0%9F%9A%9B-imperial-cargo-vtc%E2%80%94-grand-opening-convoy%F0%9F%91%91"
+    }
+];
