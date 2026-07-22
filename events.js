caa6137853adbf0eb009ff67fa39efe6e310526a
xxca11umxx <theeabros17@gmail.com>
2026-07-22 05:32:03 +0100

    Update events.js

--- events.js
+++ events.js
@@ -1,18 +1,1 @@
-const EVENTS = [
-    {
-        "title": "ETS2MCG 10th Anniversary Convoy",
-        "date": "2026-08-09T13:00:00",
-        "server": "ETS2MCG 10th Anniversary Convoy",
-        "route": "Salzburg to TruckersMP HQ | Nuremberg to TruckersMP HQ | Munich to TruckersMP HQ",
-        "banner": "",
-        "link": "https://truckersmp.com/events/33759-ets2mcg10th-anniversary-convoy"
-    },
-    {
-        "title": "Imperial Cargo  ETS2  Convoy  To be determined  8  3",
-        "date": "2026-07-29T13:00:00",
-        "server": "Unknown",
-        "route": "Check TruckersMP Event",
-        "banner": "",
-        "link": "https://truckersmp.com/events/35582-%F0%9F%9A%9B-imperial-cargo-vtc%E2%80%94-grand-opening-convoy%F0%9F%91%91"
-    }
-];
+
