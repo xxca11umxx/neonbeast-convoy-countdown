65b22003a924b0a47c4cac1e74f43e1001a811df
xxca11umxx <theeabros17@gmail.com>
2026-07-22 04:50:58 +0100

    Automatic convoy update

--- events.js
+++ events.js
@@ -1,21 +1,34 @@
 const EVENTS = [
-
     {
-        title: "ETS2MCG 10th Anniversary Convoy",
-        date: "2026-08-09T13:00:00",
-        server: "ETS2MCG 10th Anniversary Convoy",
-        route: "Salzburg → TruckersMP HQ",
-        banner: "",
-        link: "https://truckersmp.com/events/33759-ets2mcg10th-anniversary-convoy"
+        "title": "Home Events ETS2MCG 10th Anniversary Convoy",
+        "date": "2026-08-09T13:00:00",
+        "server": "ETS2MCG 10th Anniversary Convoy",
+        "route": "Salzburg to TruckersMP HQ | Nuremberg to TruckersMP HQ | Munich to TruckersMP HQ",
+        "banner": "",
+        "link": "https://truckersmp.com/events/33759-ets2mcg10th-anniversary-convoy"
     },
-
     {
-        title: "NeonBeast Logistics Summer Convoy",
-        date: "2026-09-01T19:00:00",
-        server: "Simulation 2",
-        route: "London → Calais",
-        banner: "",
-        link: "TMP LINK"
+        "title": "Home Events ETS2MCG 10th Anniversary Convoy",
+        "date": "2026-08-09T13:00:00",
+        "server": "ETS2MCG 10th Anniversary Convoy",
+        "route": "Salzburg to TruckersMP HQ | Nuremberg to TruckersMP HQ | Munich to TruckersMP HQ",
+        "banner": "",
+        "link": "https://truckersmp.com/events/33759-ets2mcg10th-anniversary-convoy"
+    },
+    {
+        "title": "Home Events NeoCrime July Convoy 2026",
+        "date": "2026-01-01T00:00:00",
+        "server": "TBD",
+        "route": "",
+        "banner": "",
+        "link": "https://truckersmp.com/events/35579-neocrime-july-convoy2026"
+    },
+    {
+        "title": "Home Events NeoCrime July Convoy 2026",
+        "date": "2026-01-01T13:00:00",
+        "server": "TBD",
+        "route": "",
+        "banner": "",
+        "link": "https://truckersmp.com/events/35579-neocrime-july-convoy2026"
     }
-
 ];
