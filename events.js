72f4e8433dab6f63cd1b49684993892df2d8901d
xxca11umxx <theeabros17@gmail.com>
2026-07-22 05:01:38 +0100

    Automatic convoy update

--- events.js
+++ events.js
@@ -38,5 +38,13 @@ const EVENTS = [
         "route": "Salzburg to TruckersMP HQ | Nuremberg to TruckersMP HQ | Munich to TruckersMP HQ",
         "banner": "",
         "link": "https://truckersmp.com/events/33759-ets2mcg10th-anniversary-convoy"
+    },
+    {
+        "title": "Home Events \ud83d\ude9b Imperial Cargo VTC \u2014 Grand Opening Convoy \ud83d\udc51",
+        "date": "2026-01-01T13:00:00",
+        "server": "Unknown",
+        "route": "",
+        "banner": "",
+        "link": "https://truckersmp.com/events/35582-%F0%9F%9A%9B-imperial-cargo-vtc%E2%80%94-grand-opening-convoy%F0%9F%91%91"
     }
 ];
