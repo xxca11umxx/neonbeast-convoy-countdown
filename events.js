32919757a2ad2da3c7a21ab148162f107df28ef8
xxca11umxx <theeabros17@gmail.com>
2026-07-22 05:17:11 +0100

    Automatic convoy update

--- events.js
+++ events.js
@@ -1,1 +1,10 @@
-
+const EVENTS = [
+    {
+        "title": "ETS2MCG   ETS2  Convoy  Event Server  259  76",
+        "date": "2026-08-09T13:00:00",
+        "server": "ETS2MCG 10th Anniversary Convoy",
+        "route": "Salzburg to TruckersMP HQ | Nuremberg to TruckersMP HQ | Munich to TruckersMP HQ",
+        "banner": "",
+        "link": "https://truckersmp.com/events/33759-ets2mcg10th-anniversary-convoy"
+    }
+];
