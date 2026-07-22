c35c55af02a352a92fc3b542c842ffe6e2b8a0c7
xxca11umxx <theeabros17@gmail.com>
2026-07-22 04:37:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,140 +1,77 @@
 let EVENT = null;
 
-
-function loadEvent(){
-
+function loadEvent() {
 
     const now = new Date();
 
-
     const upcoming = EVENTS
-
-        .filter(event => {
-
-            return new Date(event.date) > now;
-
-        })
-
-
-        .sort((a,b)=>{
-
-            return new Date(a.date) - new Date(b.date);
-
-        });
-
+        .filter(event => new Date(event.date) > now)
+        .sort((a, b) => new Date(a.date) - new Date(b.date));
 
 
     EVENT = upcoming[0];
 
 
-
-    if(!EVENT){
-
-        document.getElementById("title").innerHTML =
-        "NO UPCOMING CONVOYS";
-
+    if (!EVENT) {
+        document.getElementById("title").innerHTML = "NO UPCOMING CONVOYS";
         return;
-
     }
 
 
-
-    document.getElementById("title").innerHTML =
-    EVENT.title;
-
-
-    document.getElementById("server").innerHTML =
-    EVENT.server;
-
-
-    document.getElementById("route").innerHTML =
-    EVENT.route;
-
-
-    document.getElementById("link").href =
-    EVENT.link;
-
-
+    document.getElementById("title").innerHTML = EVENT.title;
+    document.getElementById("server").innerHTML = EVENT.server;
+    document.getElementById("route").innerHTML = EVENT.route;
+    document.getElementById("link").href = EVENT.link;
 
 }
 
 
+function updateCountdown() {
 
-function updateCountdown(){
-
-
-    if(!EVENT)
-        return;
-
+    if (!EVENT) return;
 
 
     const now = new Date();
-
-
     const eventDate = new Date(EVENT.date);
 
+    const difference = eventDate - now;
 
 
-    const difference =
-    eventDate - now;
-
-
-
-    if(difference <= 0){
-
+    if (difference <= 0) {
 
         document.getElementById("countdown").innerHTML =
         "CONVOY STARTED!";
 
-
         loadEvent();
-
         return;
-
     }
 
 
-
-    const days =
-    Math.floor(
-        difference / (1000*60*60*24)
+    const days = Math.floor(
+        difference / (1000 * 60 * 60 * 24)
     );
 
-
-    const hours =
-    Math.floor(
-        difference / (1000*60*60)%24
+    const hours = Math.floor(
+        (difference / (1000 * 60 * 60)) % 24
     );
 
-
-    const minutes =
-    Math.floor(
-        difference / (1000*60)%60
+    const minutes = Math.floor(
+        (difference / (1000 * 60)) % 60
     );
 
-
-    const seconds =
-    Math.floor(
-        difference / 1000%60
+    const seconds = Math.floor(
+        (difference / 1000) % 60
     );
-
 
 
     document.getElementById("countdown").innerHTML =
-
     `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
 }
 
 
-
 loadEvent();
 
-
-setInterval(
-    updateCountdown,
-    1000
-);
-
+setInterval(updateCountdown, 1000);
 
 updateCountdown();
